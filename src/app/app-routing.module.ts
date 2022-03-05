d36ddbae95a38e86a4cfe7cebf5256df01f38ffc
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-03-05 20:25:22 +0100

    Add routing - Add routes to home, book list, book details   (book details shows no content currently)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { BookListComponent} from './book-list/book-list.component';
+import { BookDetailsComponent} from './book-details/book-details.component';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core'; //??
+import { RouterModule, Routes } from '@angular/router'; //??
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'books',
+    component: BookListComponent
+  },
+  {
+    path: 'books/:isbn',
+    component: BookDetailsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
